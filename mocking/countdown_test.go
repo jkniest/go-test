56f98dbeb38d319045b87290626b221c2e0ccdc3
Jordan Kniest <j.kniest@basecom.de>
2024-04-28 14:10:06 +0200

    branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: # new file:  concurrency/check.go # new file: concurrency/check_test.go # modified:  mocking/countdown_test.go # Finish concurrency chapter

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -70,7 +70,7 @@ func TestConfigurableSleeper(t *testing.T) {
 	sleepTime := 5 * time.Second
 	spyTime := &SpyTime{}
 	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
-	sleeper.Sleep(sleepTime)
+	sleeper.Sleep()
 
 	if spyTime.durationSlept != sleepTime {
 		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
